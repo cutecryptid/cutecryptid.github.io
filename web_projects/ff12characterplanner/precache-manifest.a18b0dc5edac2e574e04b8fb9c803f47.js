c74b71f09be97598b667ee62ede989770486dfb2
cryptobotanist <alex.anthems@gmail.com>
2020-04-24 21:14:03 +0200

    Added FF12 Character Planner Fork

---
+++ web_projects/ff12characterplanner/precache-manifest.a18b0dc5edac2e574e04b8fb9c803f47.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cad206ae82fe754aa6f0394174ed835",
+    "url": "/web_projects/ff12characterplanner/index.html"
+  },
+  {
+    "revision": "4e0377aa4d82383a2dc5",
+    "url": "/web_projects/ff12characterplanner/static/css/2.659bcfad.chunk.css"
+  },
+  {
+    "revision": "30e1c7f042e9e5138e41",
+    "url": "/web_projects/ff12characterplanner/static/css/main.e0c05485.chunk.css"
+  },
+  {
+    "revision": "4e0377aa4d82383a2dc5",
+    "url": "/web_projects/ff12characterplanner/static/js/2.d27942dd.chunk.js"
+  },
+  {
+    "revision": "d8bff3007be2df38eb00f05e542abbda",
+    "url": "/web_projects/ff12characterplanner/static/js/2.d27942dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30e1c7f042e9e5138e41",
+    "url": "/web_projects/ff12characterplanner/static/js/main.e06c8114.chunk.js"
+  },
+  {
+    "revision": "8f4b5434b53767b49de4",
+    "url": "/web_projects/ff12characterplanner/static/js/runtime-main.1f110b70.js"
+  },
+  {
+    "revision": "08291a1c17e7f411e845dc77127548c1",
+    "url": "/web_projects/ff12characterplanner/static/media/Aquarius.08291a1c.svg"
+  },
+  {
+    "revision": "4378bbd96aca1517c543dae8e63bbeae",
+    "url": "/web_projects/ff12characterplanner/static/media/Aries.4378bbd9.svg"
+  },
+  {
+    "revision": "0a707a937707b8f8c51c512f1d24b6a5",
+    "url": "/web_projects/ff12characterplanner/static/media/Cancer.0a707a93.svg"
+  },
+  {
+    "revision": "2b0112cbb92536fee17d8f2c38facc9b",
+    "url": "/web_projects/ff12characterplanner/static/media/Capricorn.2b0112cb.svg"
+  },
+  {
+    "revision": "8a7edacfcfec6029a326f69f1053202b",
+    "url": "/web_projects/ff12characterplanner/static/media/Gemini.8a7edacf.svg"
+  },
+  {
+    "revision": "0cb0ecbea70f094f382104e288ecca28",
+    "url": "/web_projects/ff12characterplanner/static/media/Leo.0cb0ecbe.svg"
+  },
+  {
+    "revision": "dc38429d0e1778effd1cf3a181c06575",
+    "url": "/web_projects/ff12characterplanner/static/media/Libra.dc38429d.svg"
+  },
+  {
+    "revision": "852a59826b91df137e2ce3572d2f9662",
+    "url": "/web_projects/ff12characterplanner/static/media/Pisces.852a5982.svg"
+  },
+  {
+    "revision": "0d987217a9175834b04580b911817efd",
+    "url": "/web_projects/ff12characterplanner/static/media/Sagittarius.0d987217.svg"
+  },
+  {
+    "revision": "3d10e116a0ab78fb9babb6f7a5894b5b",
+    "url": "/web_projects/ff12characterplanner/static/media/Scorpio.3d10e116.svg"
+  },
+  {
+    "revision": "66b60428e77e5cb0aa44d6854e991165",
+    "url": "/web_projects/ff12characterplanner/static/media/Taurus.66b60428.svg"
+  },
+  {
+    "revision": "031563fa283811685e0c7d2c1f522bb6",
+    "url": "/web_projects/ff12characterplanner/static/media/Virgo.031563fa.svg"
+  }
+]);
